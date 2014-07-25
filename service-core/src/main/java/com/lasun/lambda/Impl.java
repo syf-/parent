1d35eb622d3748b900328560d6f259a7b82f9329
work1 <18611725826@qq.com>
2014-07-25 15:13:17 +0800

    jdk8特性练习

--- service-core/src/main/java/com/lasun/lambda/Impl.java
+++ service-core/src/main/java/com/lasun/lambda/Impl.java
@@ -3,11 +3,30 @@ package com.lasun.lambda;
 /**
  * Created by lasune40 on 2014/7/24 0024.
  */
-public class Impl implements I1
-//        , I2
-{
+interface I1 {
+    public default void print() {
+        System.out.println("I1");
+    }
+
+    public void hello();
+}
+
+interface I2 {
+    public default void print() {
+        System.out.println("I1");
+    }
+
+    public void world();
+}
+
+public class Impl implements I1, I2 {
+    public void print() {
+        I1.super.print();
+    }
+
     public void hello() {
     }
+
 
     public void world() {
     }
---
+++ service-core/src/main/java/com/lasun/lambda/ImplTest.java
@@ -0,0 +1,11 @@
+package com.lasun.lambda;
+
+/**
+ * Created by lasune40 on 2014/7/24 0024.
+ */
+public class ImplTest {
+    public static void main(String[] args) {
+        Impl m = new Impl();
+        m.print();
+    }
+}
